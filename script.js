f07f7ec5b3f9b7be7d00decb40a263dcb46c2a8a
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2025-12-17 14:38:02 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const gameboard = (function() {
+    let gameboardStorage = ["", "", "", "", "", "", "" , "", ""];
+
+
+    return {
+
+
+    }
+})();
+
+// using IIFE for one instance and private variables made immediately
